fe8409e8e74a671b08767c6778aad87a44770ffb
leontinepaq <leontine.paquatte@gmail.com>
2024-12-02 19:42:52 +0100

    ajout de t_response + test avec erreur 405 pour methode get

--- include/AMethod.hpp
+++ include/AMethod.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   AMethod.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeada-si <jeada-si@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 13:54:17 by lpaquatt          #+#    #+#             */
-/*   Updated: 2024/12/02 13:54:25 by jeada-si         ###   ########.fr       */
+/*   Updated: 2024/12/02 19:00:08 by lpaquatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,21 @@
 # define AMETHOD_HPP
 # include "Request.hpp"
 # include "Config.hpp"
-# include "Invalid.hpp"
+# include "utils.hpp"
+
+typedef struct s_statusLine
+{
+	std::string 	httpVersion;
+	int				code;
+	std::string		reasonPhrase;	
+}					t_statusLine;
+
+typedef	struct s_response
+{
+	t_statusLine				statusLine;
+	t_headers					headers;
+	t_body						body;
+}								t_response;
 
 class AMethod
 {
@@ -22,19 +36,27 @@ class AMethod
 		t_reqLine				_reqLine;
 		t_headers				_headers;
 		t_body					_body;
+		t_response				_response;
 		Config 					_config;
-		std::string				_response;
+		
+		
+		int						setResponseCode(int code, std::string response);
+
 	public:
 								AMethod() {};
 								AMethod(Config & config,  Request & request);
 		virtual 				~AMethod() {};
-		virtual std::string		response() = 0;
+		std::string				buildResponse();
+		virtual std::string		getResponse() = 0;
 
-		virtual int				validateMethod();
+		bool					isValid();
+		int						validateMethod();
 		int						validateReqURI();
 		int						validateHttpVersion();
 		std::string				errorResponse();
 };
+
+# include "Invalid.hpp"
 
 #endif
 
--- srcs/methods/AMethod.cpp
+++ srcs/methods/AMethod.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   AMethod.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeada-si <jeada-si@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 19:21:14 by lpaquatt          #+#    #+#             */
-/*   Updated: 2024/12/02 11:54:31 by jeada-si         ###   ########.fr       */
+/*   Updated: 2024/12/02 19:33:24 by lpaquatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,40 @@ AMethod::AMethod(Config & config, Request & request):
 	_body(request.getBody()),
 	_config(config)
 {
+	_response.statusLine =(t_statusLine){"HTTP/1.1", 200, "OK"};
+	_response.headers["Date"] = getDate();
+	_response.headers["Connection"] = "keep-alive"; // ou "close" mais je sais pas comment je le sais
+	// _response.headers["Connection"] = "close"; // -> client renvoie une request vide et la connection se ferme
+}
+
+int AMethod::setResponseCode(int code, std::string message) //avant de gerer mieux les erreurs
+{
+	_response.statusLine.code = code;
+	_response.statusLine.reasonPhrase = message; //todo attention aux mauvais messages pour l'instant, faire un dictionnaire selon le code ..?
+	if (code != 200)
+		putError(message, code);
+	return code;
+}
+
+bool AMethod::isValid()
+{
+	if (validateReqURI()
+		|| validateMethod()
+		|| validateHttpVersion())
+		return false;
+	else return true;
 }
 
 int AMethod::validateMethod() // + verifier selon la config
 {
 	if (_reqLine.method == INVALID)
-		return 501; //setResponseCode(501, "Method not implemented"); //a gerer
+		return setResponseCode(501, "Method not implemented");
+	// if (!_config.isMethodAllowed(_reqLine.reqURI, _reqLine.method)) // a implementer
+	if (_reqLine.method == GET) // pour test
+	{
+		_response.headers["Allow"] = "POST";
+		return setResponseCode(405, "Method not allowed");
+	}
 	return 0;
 }
 
@@ -72,4 +100,55 @@ int AMethod::validateHttpVersion()
 	if (httpVersion != "HTTP/1.1")
 		return 505; // setResponseCode(505, "HTTP Version Not Supported");
 	return 0;
+}
+
+static	std::string getErrorBody(int code)
+{
+	const std::string errorDir = "./default_errors/";
+	std::ostringstream filePath;
+	filePath << errorDir << code << ".html";
+	std::ifstream file(filePath.str().c_str());
+	if (!file.is_open()) {
+		throw std::runtime_error("Error: Cannot open error response file: " + filePath.str()); //todo: error management ?
+	}
+	std::ostringstream content;
+	content << file.rdbuf();
+	file.close();
+	return content.str();
+}
+
+//todo : gerer les messages selon la config
+std::string	AMethod::errorResponse()
+{
+	if (_response.statusLine.code != 400
+		&& _response.statusLine.code != 405
+		&& _response.statusLine.code != 501)
+	{
+		putError("errorResponse: unknown error code"); // todo: changer la verif.. faire un dictionnaire des erreurs..?
+		return "";
+	}
+	_response.body = getErrorBody(_response.statusLine.code);
+	_response.headers["Content-Type"] = "text/html; charset=UTF-8";
+	_response.headers["Content-Length"] = to_string(_response.body.size());
+	return buildResponse();
+}
+
+std::string	AMethod::buildResponse()
+{
+	std::ostringstream responseStream;
+
+	//verifier si la reponse est valide ?? 
+	responseStream << _response.statusLine.httpVersion << " "
+				<< _response.statusLine.code << " "
+				<< _response.statusLine.reasonPhrase << "\r\n";
+	if (_response.headers.find("Content-Length") == _response.headers.end())
+		_response.headers["Content-Length"] = to_string(_response.body.size());
+
+	for (t_headers::const_iterator it = _response.headers.begin(); it != _response.headers.end(); ++it) 
+		responseStream << it->first << ": " << it->second << "\r\n";
+
+	responseStream << "\r\n";
+	responseStream << _response.body;
+
+	return responseStream.str();
 }
