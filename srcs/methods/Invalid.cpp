141c27cd55915ff6bbf76a9b7ec3546dccc1c0e4
leontinepaq <leontine.paquatte@gmail.com>
2024-12-03 17:22:08 +0100

    always invallid

--- srcs/methods/Invalid.cpp
+++ srcs/methods/Invalid.cpp
@@ -6,7 +6,7 @@
 /*   By: jeada-si <jeada-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 14:40:25 by lpaquatt          #+#    #+#             */
-/*   Updated: 2024/12/05 15:22:54 by jeada-si         ###   ########.fr       */
+/*   Updated: 2024/12/06 08:34:46 by jeada-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,13 @@
 
 Invalid::Invalid(Config *config,  Request & request):
 	AMethod::AMethod(config, request)
-	{
-		
-	}
-
+{
+}
 
 std::string Invalid::getResponse()
 {
-	if (!isValid())
-		return errorResponse();
-	
-	std::string out;
-	
-	out = "HTTP/1.1 200 OK" CRLF
-		"Content-Type: text/plain" CRLF
-		"Content-Length: 19" CRLF
-		"" CRLF
-		"Test : good Request" CRLF;
-	return out;
+	// if (!isValid()) //toujours invalid
+	// 	return errorResponse();
+	isValid();
+	return errorResponse();
 }
